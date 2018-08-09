db5b7c4eecbfa4c7e3e18d28e8a2ad42d0a90385
gusbeca <gusbeca@gmail.com>
2018-08-08 20:10:10 -0500

    heritance

--- DjangoWebProject1/HelloDjangoApp/views.py
+++ DjangoWebProject1/HelloDjangoApp/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render   # Added for this step
+from datetime import datetime
+def index(request):
+    now = datetime.now()
 
-# Create your views here.
+    return render(
+        request,
+        "HelloDjangoApp/index.html",  # Relative path from the 'templates' folder to the template file
+        {
+            'title' : "Hello Django",
+            'message' : "Hello Django!",
+            'content' : " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
+
+def about(request):
+    return render(
+        request,
+        "HelloDjangoApp/about.html",
+        {
+            'title' : "About HelloDjangoApp",
+            'content' : "Example app page for Django."
+        }
+    )
